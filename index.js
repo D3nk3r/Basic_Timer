ef1e56426ee42dd5a659b5174913e3dfede0f1f9
Filip Hudec <filipveru@gmail.com>
2024-03-31 17:08:14 +0200

    commit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             clearInterval(startTimer);
         }
     }
+    
 
     function StartTimer() {
         if (!timerRunning) {
